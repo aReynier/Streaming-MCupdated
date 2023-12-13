3870d69e12aaef91e14ab2bb648f6be495916d5e
MarsaColombo <timbiaou@gmail.com>
2023-12-13 15:14:34 +0100

    Streaming Project Initial Commit

---
+++ DB.session.sql
@@ -0,0 +1,7 @@
+INSERT INTO directors (director_id, first_name, last_name, modified_at)
+VALUES (
+    director_id:integer,
+    'first_name:character varying',
+    'last_name:character varying',
+    'modified_at:timestamp without time zone'
+  );
