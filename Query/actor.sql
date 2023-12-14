6562d9ed782412fcb849f57ba9fca90e893346d3
MarsaColombo <timbiaou@gmail.com>
2023-12-14 14:58:12 +0100

    feat: :art: Cleaning the ddl for each table, and ensured to have not null and creat at and update at constraints

--- Query/actor.sql
+++
@@ -1,9 +0,0 @@
--- Active: 1702464302277@@localhost@5000@Stream@public
---Query: SELECT title, release_date FROM public.movie ORDER BY release_date DESC;
-SELECT first_name, last_name, EXTRACT(YEAR FROM AGE(NOW(), birth_date)) AS age
-FROM public.actor
-WHERE EXTRACT(YEAR FROM AGE(NOW(), birth_date)) > 30
-ORDER BY last_name, first_name;
-
-
-
--- Query/movie.sql
+++
@@ -1,3 +0,0 @@
--- Active: 1702464302277@@localhost@5000@Stream@public
--- Type: Query
-SELECT title, release_date FROM public.movie ORDER BY release_date DESC;
